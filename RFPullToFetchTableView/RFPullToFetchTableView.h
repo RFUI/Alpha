133f8fc427f872e36bcd89f8158747fbbe946e5f
BB9z <bb9z@myopera.com>
2013-07-09 11:07:32 +0800

    Format properties.

--- RFPullToFetchTableView/RFPullToFetchTableView.h
+++ RFPullToFetchTableView/RFPullToFetchTableView.h
@@ -27,34 +27,34 @@ typedef enum {
 <RFInitializing>
 
 // Default YES.
-@property (assign, nonatomic, getter = isHeaderFetchingEnabled) BOOL headerFetchingEnabled;
-@property (assign, nonatomic, getter = isFooterFetchingEnabled) BOOL footerFetchingEnabled;
+@property(assign, nonatomic, getter = isHeaderFetchingEnabled) BOOL headerFetchingEnabled;
+@property(assign, nonatomic, getter = isFooterFetchingEnabled) BOOL footerFetchingEnabled;
 
 #pragma mark - Layout
 
-@property (RF_WEAK, nonatomic) UIView *headerContainer;
-@property (RF_WEAK, nonatomic) UIView *footerContainer;
+@property(weak, nonatomic) UIView *headerContainer;
+@property(weak, nonatomic) UIView *footerContainer;
 
-@property (assign, nonatomic) RFAutoFetchTableContainerStyle headerStyle;
-@property (assign, nonatomic) RFAutoFetchTableContainerStyle footerStyle;
+@property(assign, nonatomic) RFAutoFetchTableContainerStyle headerStyle;
+@property(assign, nonatomic) RFAutoFetchTableContainerStyle footerStyle;
 
 - (void)setHeaderContainerVisible:(BOOL)isVisible animated:(BOOL)animated;
 - (void)setFooterContainerVisible:(BOOL)isVisible animated:(BOOL)animated;
 
-@property (copy, nonatomic) void (^headerVisibleChangeBlock)(BOOL isVisible, CGFloat visibleHeight, BOOL isCompleteVisible, BOOL isProccessing);
+@property(copy, nonatomic) void (^headerVisibleChangeBlock)(BOOL isVisible, CGFloat visibleHeight, BOOL isCompleteVisible, BOOL isProccessing);
 - (void)setHeaderVisibleChangeBlock:(void (^)(BOOL isVisible, CGFloat visibleHeight, BOOL isCompleteVisible, BOOL isProccessing))headerVisibleChangeBlock;
 
 @property(copy, nonatomic) void (^footerVisibleChangeBlock)(BOOL isVisible, CGFloat visibleHeight, BOOL isCompleteVisible, BOOL isProccessing, BOOL reachEnd);
 - (void)setFooterVisibleChangeBlock:(void (^)(BOOL isVisible, CGFloat visibleHeight, BOOL isCompleteVisible, BOOL isProccessing, BOOL reachEnd))footerVisibleChangeBlock;
 
 #pragma mark - Event
-@property (readonly, getter = isFetching, nonatomic) BOOL fetching;
+@property(readonly, getter = isFetching, nonatomic) BOOL fetching;
 
-@property (readonly, getter = isHeaderProcessing, nonatomic) BOOL headerProcessing;
-@property (readonly, getter = isFooterProcessing, nonatomic) BOOL footerProcessing;
+@property(readonly, getter = isHeaderProcessing, nonatomic) BOOL headerProcessing;
+@property(readonly, getter = isFooterProcessing, nonatomic) BOOL footerProcessing;
 
-@property (copy, nonatomic) void (^headerProccessBlock)(void);
-@property (copy, nonatomic) void (^footerProccessBlock)(void);
+@property(copy, nonatomic) void (^headerProccessBlock)(void);
+@property(copy, nonatomic) void (^footerProccessBlock)(void);
 
 // Call them method to notice proccess is finished.
 - (void)footerProccessFinshed;
@@ -68,10 +68,10 @@ typedef enum {
 @property(assign, nonatomic) BOOL footerReachEnd;
 
 // Default `YES`
-@property (assign, nonatomic) BOOL shouldScrollToTopWhenHeaderEventTrigged;
+@property(assign, nonatomic) BOOL shouldScrollToTopWhenHeaderEventTrigged;
 
 // Default `YES`, not work rightnow.
-@property (assign, nonatomic) BOOL shouldScrollToLastVisibleRowBeforeTriggeAfterFooterProccessFinished;
+@property(assign, nonatomic) BOOL shouldScrollToLastVisibleRowBeforeTriggeAfterFooterProccessFinished;
 
 // Default `NO`
 //@property (assign, nonatomic) BOOL shouldScrollToBottomWhenFooterEventTrigged;

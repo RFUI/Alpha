5aac8d61e53748569fafbefefed13fc68c85e69c
BB9z <bb9z@me.com>
2014-01-06 16:14:58 +0800

    New UIButton+RFResizableBackgroundImage.

---
+++ RFBackground/UIButton+RFResizableBackgroundImage.h
@@ -0,0 +1,64 @@
+/*!
+    UIButton (RFResizableBackgroundImage)
+    RFUI
+
+    Copyright (c) 2014 BB9z
+    https://github.com/RFUI/Alpha
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+
+    Alpha
+ */
+
+#import "RFUI.h"
+
+/**
+ @category UIButton (RFResizableBackgroundImage)
+
+ This category help you making button’s background image resizable if you need support iOS 6 devices.
+
+ If you use storyboard and only have several buttons, you can set these edge insets using user defined runtime attributes.
+ 
+ If there are many buttons, I advice you set them using UIAppearance, eg.
+
+ @code
+ @implementation MyButton
+
+ + (void)load {
+     [[self appearance] setBackgroundImageResizingCapInsets:UIEdgeInsetsMake(0, 5, 0, 5)];
+     [[self appearance] setBackgroundImageResizingCapInsets2x:UIEdgeInsetsMake(0, 3.5, 0, 3.5)];
+ }
+
+ @end
+
+ @endcode
+ */
+@interface UIButton (RFResizableBackgroundImage)
+
+/** Background image resizing cap insets.
+ 
+ @discussion Generally, you set this property to make current background image resizable. It will change background image for normal, highlighted, selected and disabled status.
+
+ @see backgroundImageResizingCapInsets2x
+ @see backgroundImageStretchResizingMode
+*/
+@property (assign, nonatomic) UIEdgeInsets backgroundImageResizingCapInsets NS_AVAILABLE_IOS(6_0) UI_APPEARANCE_SELECTOR;
+
+/** Resizing cap insets for retina display.
+
+ @discussion If your 2x and 1x image has the same resizing cap insets, set this property is not required.
+ 
+ @see backgroundImageResizingCapInsets
+ @see backgroundImageStretchResizingMode
+ */
+@property (assign, nonatomic) UIEdgeInsets backgroundImageResizingCapInsets2x NS_AVAILABLE_IOS(6_0) UI_APPEARANCE_SELECTOR;
+
+/** Background image resizing mode.
+ 
+ @discussion If set `YES`, the background image will be stretched when it is resized. Otherwise, it will be tiled.
+ */
+@property (assign, nonatomic) BOOL backgroundImageStretchResizingMode NS_AVAILABLE_IOS(6_0) UI_APPEARANCE_SELECTOR;
+
+
+@end
---
+++ RFBackground/UIButton+RFResizableBackgroundImage.m
@@ -0,0 +1,70 @@
+
+#import "UIButton+RFResizableBackgroundImage.h"
+#import "RFRuntime.h"
+#import <objc/runtime.h>
+#import "UIDevice+RFKit.h"
+
+static char UIButtonBackgroundImageResizingCapInsetsCateogryProperty;
+static char UIButtonBackgroundImageResizingCapInsets2xCateogryProperty;
+static char UIButtonBackgroundImageStretchResizingModeCateogryProperty;
+
+@implementation UIButton (RFResizableBackgroundImage)
+
+- (UIEdgeInsets)backgroundImageResizingCapInsets {
+    return [objc_getAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsetsCateogryProperty) UIEdgeInsetsValue];
+}
+
+- (UIEdgeInsets)backgroundImageResizingCapInsets2x {
+    return [objc_getAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsets2xCateogryProperty) UIEdgeInsetsValue];
+}
+
+- (BOOL)backgroundImageStretchResizingMode {
+    return [objc_getAssociatedObject(self, &UIButtonBackgroundImageStretchResizingModeCateogryProperty) boolValue];
+}
+
+- (void)setBackgroundImageStretchResizingMode:(BOOL)backgroundImageStretchResizingMode {
+    objc_setAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsetsCateogryProperty, @(backgroundImageStretchResizingMode), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+#define __UIButtonSetResizableBackgroundImageForState(IX, STATE)\
+    tmp = [self backgroundImageForState:STATE];\
+    if (tmp) {\
+        [self setBackgroundImage:[tmp resizableImageWithCapInsets:backgroundImageResizingCapInsets resizingMode:mode] forState:STATE];\
+    }
+
+- (void)setBackgroundImageResizingCapInsets:(UIEdgeInsets)backgroundImageResizingCapInsets {
+    objc_setAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsetsCateogryProperty, [NSValue valueWithUIEdgeInsets:backgroundImageResizingCapInsets], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+    if ([UIDevice currentDevice].isRetinaDisplay && objc_getAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsets2xCateogryProperty)) {
+        return;
+    }
+    else {
+        UIImage *tmp;
+        UIImageResizingMode mode = self.backgroundImageStretchResizingMode? UIImageResizingModeStretch : UIImageResizingModeTile;
+        metamacro_foreach(__UIButtonSetResizableBackgroundImageForState,,
+                          UIControlStateNormal,
+                          UIControlStateHighlighted,
+                          UIControlStateSelected,
+                          UIControlStateDisabled);
+    }
+}
+
+- (void)setBackgroundImageResizingCapInsets2x:(UIEdgeInsets)backgroundImageResizingCapInsets {
+    objc_setAssociatedObject(self, &UIButtonBackgroundImageResizingCapInsets2xCateogryProperty, [NSValue valueWithUIEdgeInsets:backgroundImageResizingCapInsets], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+    if (![UIDevice currentDevice].isRetinaDisplay) {
+        return;
+    }
+    else {
+        UIImage *tmp;
+        UIImageResizingMode mode = self.backgroundImageStretchResizingMode? UIImageResizingModeStretch : UIImageResizingModeTile;
+        metamacro_foreach(__UIButtonSetResizableBackgroundImageForState,,
+                          UIControlStateNormal,
+                          UIControlStateHighlighted,
+                          UIControlStateSelected,
+                          UIControlStateDisabled);
+    }
+}
+
+#undef __UIButtonSetResizableBackgroundImageForState
+@end

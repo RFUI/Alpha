7e091edf08c527b96c549930756a515f267527cd
BB9z <bb9z@me.com>
2014-01-12 15:53:37 +0800

    New RFApperanceLayerCornerRadius category to change a view layer’s corner radius.

---
+++ RFViewApperance/UIView+RFApperanceLayerCornerRadius.h
@@ -0,0 +1,18 @@
+/*!
+    UIView (RFApperanceLayerCornerRadius)
+    RFUI
+
+    Copyright (c) 2014 BB9z
+    https://github.com/RFUI/Alpha
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+
+    Alpha
+ */
+
+#import "RFUI.h"
+
+@interface UIView (RFApperanceLayerCornerRadius)
+@property CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
+@end

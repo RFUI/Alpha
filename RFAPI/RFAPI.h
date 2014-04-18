7454e86d9dbe4a29d998c45d25f1f9c258064512
BB9z <bb9z@me.com>
2014-04-18 15:46:18 +0800

    Dump process.

--- RFAPI/RFAPI.h
+++ RFAPI/RFAPI.h
@@ -1,11 +1,20 @@
 // TEST
 
+#import "RFUI.h"
 #import "RFAPIDefine.h"
-#import "RFInitializing.h"
 #import "AFNetworkReachabilityManager.h"
 #import "AFURLRequestSerialization.h"
+#import "JSONModel.h"
 
-@class RFMessageManager;
+/**
+ TODO: 
+ [ ] 文件上传
+ [ ] 缓存实现
+ [ ] 加载状态集成
+
+ */
+
+@class RFMessageManager, AFHTTPRequestOperation;
 @protocol AFURLResponseSerialization;
 
 @interface RFAPI : NSOperationQueue <
@@ -14,17 +23,39 @@
 
 + (instancetype)sharedInstance;
 
-@property (copy, nonatomic) NSURL *baseURL;
+@property (copy, nonatomic) NSURL *baseURL DEPRECATED_ATTRIBUTE;
 
 @property (readonly, nonatomic) AFNetworkReachabilityManager *reachabilityManager;
 
 #pragma mark - Define
-
+/// Load API deine config file.
 - (void)setAPIDefineWithRules:(NSDictionary *)rules;
 
+/// Get an API define object with API name.
 - (RFAPIDefine *)defineForName:(NSString *)APIName;
 
+#pragma mark - Request management
+
+// No implementation
+- (void)cancelOperationWithIdentifier:(NSString *)identifier;
+
+// No implementation
+- (void)cancelOperationsWithGroupIdentifier:(NSString *)identifier;
+
 #pragma mark - Request
+
+// 如果传一个特殊请求，直接创建一个 AFHTTPRequestOperation 并加进来也许更合适
+
+// No implementation
+- (void)requestWithName:(NSString *)APIName
+             identifier:(NSString *)identifier
+        groupIdentifier:(NSString *)groupIdentifier
+             parameters:(NSDictionary *)parameters
+        uploadResources:(NSArray *)uploadResources
+                headers:(NSDictionary *)headers
+                success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
+                failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
+             completion:(void (^)(AFHTTPRequestOperation *operation))completion;
 
 @property (strong, nonatomic) AFHTTPRequestSerializer<AFURLRequestSerialization> *requestSerializer;
 
@@ -56,7 +87,6 @@
 
  @see AFMultipartFormData
  */
-- (NSMutableURLRequest *)URLRequestWithDefine:(RFAPIDefine *)define parameters:(NSDictionary *)parameters uploadResources:(NSArray *)uploadResources;
 
 /**
  For overwrite, default do nothing.
@@ -78,4 +108,48 @@
  */
 - (void)alertError:(NSError *)error title:(NSString *)title;
 
+#pragma mark - Old raw request
+
+/** 创建并执行请求，期望的返回是一个数组
+
+ @param method      HTTP 请求模式
+ @param URLString   请求的相对地址
+ @param parameters  请求参数，可为空
+ @param headers     附加的 HTTP header，可为空
+ @param modelClass  数组中期望的元素的 JSONModel 类型
+ @param success     请求成功回调的 block，可为空
+ @param failure     请求失败回调的 block，可为空
+ @param completion  请求完成回掉的 block，必定会被调用（即使请求创建失败），会在 success 和 failure 回调后执行。被设计用来执行通用的清理。可为空
+ */
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers expectArrayContainsClass:(Class)modelClass success:(void (^)(AFHTTPRequestOperation *operation, NSMutableArray *objects))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion;
+
+/** 创建并执行请求，期望的返回是一个对象
+
+ @param modelClass 期望的元素的 JSONModel 类型
+ */
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers expectObjectClass:(Class)modelClass success:(void (^)(AFHTTPRequestOperation *operation, id JSONModelObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion;
+
+/** 创建并执行请求
+
+ @return AFHTTPRequestOperation 对象
+ */
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion;
+
+/** 创建 NSURLRequest 对象
+
+ @param method HTTP 请求模式，如 `GET`、`POST`。不能为空
+ @param URLString 请求路径，相对于 baseURL。不能为空
+ @param parameters HTTP 请求的参数
+ @param headers 附加的 HTTP header，新加的字段的会覆盖原有的
+ @param error 创建请求对象出错时产生的错误
+
+ @return 使用当前对象的 requestSerializer 序列好的请求对象
+ */
+- (NSMutableURLRequest *)URLRequestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers error:(NSError *__autoreleasing *)error;
+
+@end
+
+extern NSString *const RFAPIErrorDomain;
+
+@interface RFHTTPRequestFormData : NSObject
 @end
--- RFAPI/RFAPI.m
+++ RFAPI/RFAPI.m
@@ -11,6 +11,12 @@
 #import "AFNetworkReachabilityManager.h"
 #import "AFNetworkActivityIndicatorManager.h"
 
+RFDefineConstString(RFAPIErrorDomain);
+
+#ifndef DebugAPIDelayFetchCallbackReturnSecond
+#   define DebugAPIDelayFetchCallbackReturnSecond 0
+#endif
+
 @interface RFAPI ()
 @property (strong, nonatomic, readwrite) AFNetworkReachabilityManager *reachabilityManager;
 @property (strong, nonatomic) RFAPIDefineManager *defineManager;
@@ -35,6 +41,7 @@ RFInitializingRootForNSObject
 }
 
 - (void)afterInit {
+    [self.reachabilityManager startMonitoring];
 }
 
 //- (NSString *)description {
@@ -52,6 +59,15 @@ RFInitializingRootForNSObject
 }
 
 #pragma mark - Request
+
+- (void)requestWithName:(NSString *)APIName parameters:(NSDictionary *)parameters uploadResources:(NSArray *)uploadResources headers:(NSDictionary *)headers success:(void (^)(AFHTTPRequestOperation *, id))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure completion:(void (^)(AFHTTPRequestOperation *))completion {
+    RFAPIDefine *define = [self defineForName:APIName];
+    if (!define) {
+
+
+        return;
+    }
+}
 
 - (AFHTTPRequestSerializer<AFURLRequestSerialization> *)requestSerializer {
     if (!_requestSerializer) {
@@ -85,6 +101,191 @@ RFInitializingRootForNSObject
     [self.networkActivityIndicatorManager alertError:error title:title];
 }
 
+#pragma mark - Old raw request
 
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers expectArrayContainsClass:(Class)modelClass success:(void (^)(AFHTTPRequestOperation *operation, NSMutableArray *objects))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion {
+    RFAssert([modelClass isSubclassOfClass:[JSONModel class]], @"modelClass 必须是 JSONModel");
+
+    return [self requestWithMethod:method URLString:URLString parameters:parameters headers:headers success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (![responseObject isKindOfClass:[NSArray class]]) {
+            if (failure) {
+#if RFDEBUG
+                dout_error(@"期望的数据类型是数组，而实际是 %@\n", [responseObject class]);
+#endif
+                failure(operation, [NSError errorWithDomain:RFAPIErrorDomain code:0 userInfo:@{
+                    NSLocalizedDescriptionKey : @"返回数据异常",
+                    NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
+                    NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
+                    NSURLErrorFailingURLErrorKey : operation.request.URL
+                }]);
+            }
+            return;
+        }
+
+        NSError __autoreleasing *e = nil;
+        NSMutableArray *objects = [NSMutableArray arrayWithCapacity:[responseObject count]];
+        for (NSDictionary *info in responseObject) {
+            id obj = [[modelClass alloc] initWithDictionary:info error:&e];
+            if (obj) {
+                [objects addObject:obj];
+            }
+            else {
+                if (failure) {
+#if RFDEBUG
+                    dout_error(@"不能将数组中的元素转换为Model %@\n请先确认一下代码，如果服务器没按要求返回请联系后台人员", e);
+#endif
+                    failure(operation, [NSError errorWithDomain:RFAPIErrorDomain code:0 userInfo:@{
+                        NSLocalizedDescriptionKey : @"返回数据异常",
+                        NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
+                        NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
+                        NSURLErrorFailingURLErrorKey : operation.request.URL
+                    }]);
+                }
+                return;
+            }
+        }
+
+        if (success) {
+            success(operation, objects);
+        }
+    } failure:failure completion:completion];
+}
+
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers expectObjectClass:(Class)modelClass success:(void (^)(AFHTTPRequestOperation *operation, id JSONModelObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion {
+    RFAssert([modelClass isSubclassOfClass:[JSONModel class]], @"modelClass 必须是 JSONModel");
+
+    return [self requestWithMethod:method URLString:URLString parameters:parameters headers:headers success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (![responseObject isKindOfClass:[NSDictionary class]]) {
+            if (failure) {
+#if RFDEBUG
+                dout_error(@"期望的数据类型是字典，而实际是 %@\n请先确认一下代码，如果服务器没按要求返回请联系后台人员", [responseObject class]);
+#endif
+                failure(operation, [NSError errorWithDomain:RFAPIErrorDomain code:0 userInfo:@{
+                    NSLocalizedDescriptionKey : @"返回数据异常",
+                    NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
+                    NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
+                    NSURLErrorFailingURLErrorKey : operation.request.URL
+                }]);
+            }
+            return;
+        }
+
+        NSError __autoreleasing *e = nil;
+        id JSONModelObject = [[modelClass alloc] initWithDictionary:responseObject error:&e];
+        if (!JSONModelObject) {
+            if (failure) {
+#if RFDEBUG
+                dout_error(@"不能将返回内容转换为Model：%@\n请先确认一下代码，如果服务器没按要求返回请联系后台人员", e);
+#endif
+                failure(operation, [NSError errorWithDomain:RFAPIErrorDomain code:0 userInfo:@{
+                    NSLocalizedDescriptionKey : @"返回数据异常",
+                    NSLocalizedFailureReasonErrorKey : @"可能服务器正在升级或者维护，也可能是应用bug",
+                    NSLocalizedRecoverySuggestionErrorKey : @"建议稍后重试，如果持续报告这个错误请检查应用是否有新版本",
+                    NSURLErrorFailingURLErrorKey : operation.request.URL
+                }]);
+            }
+            return;
+        }
+
+        if (success) {
+            success(operation, JSONModelObject);
+        }
+    } failure:failure completion:completion];
+}
+
+#define __APICompletionCallback(BLOCK, OPERATION, OBJECT)\
+    if (BLOCK) {\
+        if (DebugAPIDelayFetchCallbackReturnSecond) {\
+            dispatch_after_seconds(DebugAPIDelayFetchCallbackReturnSecond, ^{\
+                BLOCK(OPERATION, OBJECT);\
+            });\
+        }\
+        else {\
+            BLOCK(OPERATION, OBJECT);\
+        }\
+    }
+
+- (AFHTTPRequestOperation *)requestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure completion:(void (^)(AFHTTPRequestOperation *operation))completion {
+    RFAssert(success, @"你确定成功没回调？");
+    RFAssert(failure, @"写个接口错误不处理？");
+
+    NSError *e = nil;
+    NSMutableURLRequest *request = [self URLRequestWithMethod:method URLString:URLString parameters:parameters headers:headers error:&e];
+    if (e) {
+        __APICompletionCallback(failure, nil, e)
+        if (completion) {
+            completion(nil);
+        }
+        return nil;
+    }
+
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = self.responseSerializer;
+
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *op, id responseObject) {
+        __APICompletionCallback(success, op, responseObject)
+        if (completion) {
+            completion(op);
+        }
+    } failure:^(AFHTTPRequestOperation *op, NSError *error) {
+        __APICompletionCallback(failure, op, error)
+        if (completion) {
+            completion(op);
+        }
+    }];
+
+    [self addOperation:operation];
+    return operation;
+}
+
+#undef __APICompletionCallback
+
+- (NSMutableURLRequest *)URLRequestWithMethod:(NSString *)method URLString:(NSString *)URLString parameters:(NSDictionary *)parameters headers:(NSDictionary *)headers error:(NSError *__autoreleasing *)error {
+    NSParameterAssert(method);
+    NSParameterAssert(URLString);
+
+    NSURL *url = [NSURL URLWithString:URLString relativeToURL:self.baseURL];
+    if (!url) {
+        if (error) {
+#if RFDEBUG
+            dout_error(@"无法拼接 URL: %@\n请检查代码是否正确", URLString);
+#endif
+            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadURL userInfo:@{
+                NSLocalizedDescriptionKey : @"内部错误，无法创建请求",
+                NSLocalizedFailureReasonErrorKey : @"很可能是应用bug",
+                NSLocalizedRecoverySuggestionErrorKey : @"请再试一次，如果依旧请尝试重启应用。给您带来不便，敬请谅解",
+                NSURLErrorFailingURLErrorKey : URLString
+            }];
+        }
+        return nil;
+    }
+    
+    NSURLRequestCachePolicy cachePolicy = (self.reachabilityManager.reachable)? NSURLRequestUseProtocolCachePolicy : NSURLRequestReturnCacheDataElseLoad;
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:cachePolicy timeoutInterval:10];
+    [request setHTTPMethod:method];
+    
+    NSError __autoreleasing *e = nil;
+    request = [[self.requestSerializer requestBySerializingRequest:request withParameters:parameters error:&e] mutableCopy];
+    if (e) {
+        if (error) {
+#if RFDEBUG
+            dout_error(@"无法序列化参数\n请检查代码是否正确");
+#endif
+            *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorCancelled userInfo:@{
+                NSLocalizedDescriptionKey : @"内部错误，无法创建请求",
+                NSLocalizedFailureReasonErrorKey : @"很可能是应用bug",
+                NSLocalizedRecoverySuggestionErrorKey : @"请再试一次，如果依旧请尝试重启应用。给您带来不便，敬请谅解",
+                NSURLErrorFailingURLErrorKey : URLString
+            }];
+        }
+        return nil;
+    }
+    
+    [headers enumerateKeysAndObjectsUsingBlock:^(id field, id value, BOOL *__unused stop) {
+        [request setValue:value forHTTPHeaderField:field];
+    }];
+    
+	return request;
+}
 
 @end

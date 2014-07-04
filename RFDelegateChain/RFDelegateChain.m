705b50685d741adb247364a8ca8fe0fa20d596b3
BB9z <bb9z@me.com>
2014-07-04 18:53:16 +0800

    Add a macro to implemente RFDelegateChain. So we can use this macro to add this feature to other class easily.

--- RFDelegateChain/RFDelegateChain.m
+++ RFDelegateChain/RFDelegateChain.m
@@ -16,45 +16,6 @@ RFInitializingRootForNSObject
     return [NSString stringWithFormat:@"<%@: %p, delegate = %@>", self.class, self, self.delegate];
 }
 
-- (BOOL)respondsToSelector:(SEL)aSelector {
-    if ([super respondsToSelector:aSelector]) {
-        return YES;
-    }
-
-    if ([self.delegate respondsToSelector:aSelector]) {
-        return YES;
-    }
-
-    return NO;
-}
-
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-    NSMethodSignature* signature = [super methodSignatureForSelector:aSelector];
-
-    if (!signature) {
-        __strong id delegate = self.delegate;
-        if ([delegate respondsToSelector:aSelector]) {
-            return [delegate methodSignatureForSelector:aSelector];
-        }
-    }
-
-    return signature;
-}
-
-- (void)forwardInvocation:(NSInvocation *)anInvocation {
-    __strong id delegate = self.delegate;
-    if ([delegate respondsToSelector:anInvocation.selector]) {
-        [anInvocation invokeWithTarget:delegate];
-    }
-}
-
-- (id)forwardingTargetForSelector:(SEL)aSelector {
-    __strong id delegate = self.delegate;
-    id target = [super forwardingTargetForSelector:aSelector];
-    if (target != delegate) {
-        return [delegate forwardingTargetForSelector:aSelector];
-    }
-    return target;
-}
+RFDelegateChainForwordMethods(super, self.delegate)
 
 @end

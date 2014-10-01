0b54a54a3433fbd98e1a4f89dcfd416b36023f0a
BB9z <bb9z@me.com>
2014-10-01 12:03:24 +0800

    Minor header modify.

--- RFDelegateChain/UIKit/UITextFiledDelegateChain.h
+++ RFDelegateChain/UIKit/UITextFiledDelegateChain.h
@@ -16,12 +16,14 @@
 @property (weak, nonatomic) IBOutlet id<UITextFieldDelegate> delegate;
 
 /// If thest property set, delegate methods wont called.
+#pragma mark Managing Editing
 @property (copy, nonatomic) BOOL (^shouldBeginEditing)(UITextField *textField, id<UITextFieldDelegate> delegate);
 @property (copy, nonatomic) void (^didBeginEditing)(UITextField *textField, id<UITextFieldDelegate> delegate);
 @property (copy, nonatomic) BOOL (^shouldEndEditing)(UITextField *textField, id<UITextFieldDelegate> delegate);
 @property (copy, nonatomic) void (^didEndEditing)(UITextField *textField, id<UITextFieldDelegate> delegate);
-@property (copy, nonatomic) BOOL (^shouldChangeCharacters)(UITextField *textField, NSRange inRange, NSString *replacementString, id<UITextFieldDelegate> delegate);
 
+#pragma mark Editing the Text Field’s Text
+@property (copy, nonatomic) BOOL (^shouldChangeCharacters)(UITextField *textField, NSRange inRange, NSString *replacementString, id<UITextFieldDelegate> delegate);
 @property (copy, nonatomic) BOOL (^shouldClear)(UITextField *textField, id<UITextFieldDelegate> delegate);
 @property (copy, nonatomic) BOOL (^shouldReturn)(UITextField *textField, id<UITextFieldDelegate> delegate);
 
--- RFDelegateChain/UIKit/UITextViewDelegateChain.h
+++ RFDelegateChain/UIKit/UITextViewDelegateChain.h
@@ -19,17 +19,21 @@
 @property (weak, nonatomic) IBOutlet id<UITextViewDelegate> delegate;
 
 /// If thest property set, delegate methods wont called.
+#pragma mark Responding to Editing Notifications
 @property (copy, nonatomic) BOOL (^shouldBeginEditing)(UITextView *textView, id<UITextViewDelegate> delegate);
-@property (copy, nonatomic) BOOL (^shouldEndEditing)(UITextView *textView, id<UITextViewDelegate> delegate);
 @property (copy, nonatomic) void (^didBeginEditing)(UITextView *textView, id<UITextViewDelegate> delegate);
+@property (copy, nonatomic) BOOL (^shouldEndEditing)(UITextView *textView, id<UITextViewDelegate> delegate);
 @property (copy, nonatomic) void (^didEndEditing)(UITextView *textView, id<UITextViewDelegate> delegate);
 
+#pragma mark Responding to Text Changes
 @property (copy, nonatomic) BOOL (^shouldChangeTextInRange)(UITextView *textView, NSRange range, NSString *replacementText, id<UITextViewDelegate> delegate);
-
 @property (copy, nonatomic) void (^didChange)(UITextView *textView, id<UITextViewDelegate> delegate);
+
+#pragma mark Responding to Selection Changes
 @property (copy, nonatomic) void (^didChangeSelection)(UITextView *textView, id<UITextViewDelegate> delegate);
 
-@property (copy, nonatomic) BOOL (^shouldInteractWithURL)(UITextView *textView, NSURL *URL, NSRange characterRange, id<UITextViewDelegate> delegate);
-@property (copy, nonatomic) BOOL (^shouldInteractWithTextAttachment)(UITextView *textView, NSTextAttachment *textAttachment, NSRange characterRange, id<UITextViewDelegate> delegate);
+#pragma mark Interacting with Text Data
+@property (copy, nonatomic) BOOL (^shouldInteractWithURL)(UITextView *textView, NSURL *URL, NSRange characterRange, id<UITextViewDelegate> delegate) NS_AVAILABLE_IOS(7_0);
+@property (copy, nonatomic) BOOL (^shouldInteractWithTextAttachment)(UITextView *textView, NSTextAttachment *textAttachment, NSRange characterRange, id<UITextViewDelegate> delegate) NS_AVAILABLE_IOS(7_0);
 
 @end

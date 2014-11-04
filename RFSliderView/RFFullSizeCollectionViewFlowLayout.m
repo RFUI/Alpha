72cdb5ec258da57483fac98337d3b8ee6b834dde
BB9z <bb9z@me.com>
2014-11-04 15:37:34 +0800

    Reverse "RFFullSizeCollectionViewFlowLayout, avoid possible crash."

--- RFSliderView/RFFullSizeCollectionViewFlowLayout.m
+++ RFSliderView/RFFullSizeCollectionViewFlowLayout.m
@@ -17,18 +17,14 @@
 }
 
 - (void)prepareLayout {
-    @synchronized(self) {
-        if (self) {
-            [super prepareLayout];
-            CGSize itemSize = self.collectionView.bounds.size;
-            if (itemSize.width > 0 && itemSize.width < 10000 && itemSize.height > 0 && itemSize.height < 10000) {
-                self.itemSize = itemSize;
-            }
-            else {
-                self.itemSize = CGSizeMake(1, 1);
-            }
-        }
+    CGSize itemSize = self.collectionView.bounds.size;
+    if (itemSize.width > 0 && itemSize.width < 10000 && itemSize.height > 0 && itemSize.height < 10000) {
+        self.itemSize = itemSize;
     }
+    else {
+        self.itemSize = CGSizeMake(1, 1);
+    }
+    [super prepareLayout];
 }
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {

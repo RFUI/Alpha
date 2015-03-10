a3b0265776dc05804125abe0b3c76e6d47651bcb
BB9z <bb9z@me.com>
2015-03-10 12:03:18 +0800

    New RFKVOWrapper.

---
+++ RFKVOWrapper/RFKVOWrapper.h
@@ -0,0 +1,65 @@
+/*!
+    RFKVOWrapper
+
+    Copyright (c) 2015 BB9z
+    https://github.com/RFUI/Alpha
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+
+    TEST
+ */
+
+/*!
+ RFKVOWrapper contains code from ReactiveCocoa v0.6.0, the use of which is hereby acknowledged.
+
+ ReactiveCocoa https://github.com/ReactiveCocoa/ReactiveCocoa
+ Copyright (c) 2011-2012, GitHub, Inc. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (RFKVOWrapper)
+
+/**
+ Adds the given block as the callback for when the keyPath changes. 
+ 
+ The observer does not need to be explicitly removed. It will be removed when the observer or observed object is dealloc'd.
+ 
+ @param observer    The object to which callbacks will be delivered. This is passed back into the given block.
+ @param keyPath     The key path to observe.
+ @param options     The key-value observing options.
+ @param queue       The queue in which the callback block should be performed. Passing nil means the block will be performed in whatever queue the observer callback came in on.
+ @param block       The block called when the value at the key path changes.
+
+ @return Returns an identifier that can be used to remove the observer.
+ */
+- (id)RFAddObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options queue:(NSOperationQueue *)queue block:(void (^)(id observer, NSDictionary *change))block;
+
+/**
+ Remove the observer represented by the identifier.
+ 
+ @param identifier  The identifier to removed. This should be an object previously returned by a called to -addObserverForKeyPath:options:queue:block:.
+ 
+ @return Whether the removal was successful. The only reason for failure would be if the identifier doesn't represent anything currently being observed by the object, or if the identifier is nil.
+ */
+- (BOOL)RFRemoveObserverWithIdentifier:(id)identifier;
+@end
---
+++ RFKVOWrapper/RFKVOWrapper.m
@@ -0,0 +1,188 @@
+
+#import "RFKVOWrapper.h"
+#import "RFSwizzle.h"
+@import ObjectiveC.runtime;
+
+typedef void (^RFKVOBlock)(id, NSDictionary *);
+
+static void *RFKVOTrampolinesKey = &RFKVOTrampolinesKey;
+static void *RFKVOWrapperContext = &RFKVOWrapperContext;
+
+static NSMutableSet *RFSwizzledClasses() {
+    static dispatch_once_t onceToken;
+    static NSMutableSet *swizzledClasses = nil;
+    dispatch_once(&onceToken, ^{
+        swizzledClasses = [[NSMutableSet alloc] init];
+    });
+
+    return swizzledClasses;
+}
+
+@interface NSObject ()
+// This set should only be manipulated while synchronized on the receiver.
+@property (nonatomic, strong) NSMutableSet *RFKVOTrampolines;
+@end
+
+@interface RFKVOTrampoline : NSObject
+
+- (id)initWithTarget:(NSObject *)target observer:(NSObject *)observer keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options queue:(NSOperationQueue *)queue block:(RFKVOBlock)block;
+
+@property (nonatomic, readonly, copy) NSString *keyPath;
+@property (nonatomic, readonly, strong) NSOperationQueue *queue;
+
+// These properties should only be manipulated while synchronized on the receiver.
+@property (nonatomic, copy) RFKVOBlock block;
+@property (nonatomic, unsafe_unretained) NSObject *target;
+@property (nonatomic, unsafe_unretained) NSObject *observer;
+
+- (void)addAsTrampolineOnObject:(NSObject *)obj;
+
+@end
+
+@implementation RFKVOTrampoline
+
+#pragma mark Lifecycle
+
+- (id)initWithTarget:(NSObject *)target observer:(NSObject *)observer keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options queue:(NSOperationQueue *)queue block:(RFKVOBlock)block {
+    self = [super init];
+    if (self == nil) return nil;
+
+    _keyPath = [keyPath copy];
+    _queue = queue;
+
+    self.block = block;
+    self.target = target;
+    self.observer = observer;
+
+    [self.target addObserver:self forKeyPath:self.keyPath options:options context:&RFKVOWrapperContext];
+    [self addAsTrampolineOnObject:self.target];
+    [self addAsTrampolineOnObject:self.observer];
+
+    return self;
+}
+
+- (void)addAsTrampolineOnObject:(NSObject *)obj {
+    @synchronized (obj) {
+        if (obj.RFKVOTrampolines == nil) {
+            obj.RFKVOTrampolines = [NSMutableSet setWithObject:self];
+        } else {
+            [obj.RFKVOTrampolines addObject:self];
+        }
+    }
+}
+
+- (void)dealloc {
+    [self stopObserving];
+}
+
+#pragma mark Observation
+
+- (void)stopObserving {
+    NSObject *target;
+    NSObject *observer;
+
+    @synchronized (self) {
+        self.block = nil;
+
+        target = self.target;
+        observer = self.observer;
+
+        self.target = nil;
+        self.observer = nil;
+    }
+
+    @synchronized (target) {
+        [target.RFKVOTrampolines removeObject:self];
+    }
+
+    @synchronized (observer) {
+        [observer.RFKVOTrampolines removeObject:self];
+    }
+
+    [target removeObserver:self forKeyPath:self.keyPath];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (context != &RFKVOWrapperContext) {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+        return;
+    }
+
+    RFKVOBlock block;
+    id observer;
+    // We need to keep the target alive until the notification's been delivered,
+    // which could be some point later in time if we're not in `queue` currently.
+    __block id target;
+
+    @synchronized (self) {
+        block = self.block;
+        observer = self.observer;
+        target = self.target;
+    }
+
+    void (^notificationBlock)(void) = ^{
+        if (block != nil) block(observer, change);
+    };
+
+    if (self.queue == nil || self.queue == [NSOperationQueue currentQueue]) {
+        notificationBlock();
+    } else {
+        [self.queue addOperationWithBlock:^{
+            notificationBlock();
+            target = nil;
+        }];
+    }
+}
+
+@end
+
+@implementation NSObject (RFKVOWrapper)
+
+- (void)_RFKVOWrapperSwizzed_dealloc {
+    NSSet *trampolines;
+
+    @synchronized (self) {
+        trampolines = [self.RFKVOTrampolines copy];
+        self.RFKVOTrampolines = nil;
+    }
+
+    // If we're currently delivering a KVO callback then niling the trampoline set might not dealloc the trampoline and therefore make them be dealloc'd. So we need to manually stop observing on all of them as well.
+    [trampolines makeObjectsPerformSelector:@selector(stopObserving)];
+
+    [self _RFKVOWrapperSwizzed_dealloc];
+}
+
+- (id)RFAddObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options queue:(NSOperationQueue *)queue block:(void (^)(id observer, NSDictionary *change))block {
+    void (^swizzle)(Class) = ^(Class classToSwizzle){
+        NSString *className = NSStringFromClass(classToSwizzle);
+        if ([RFSwizzledClasses() containsObject:className]) return;
+
+        RFSwizzleInstanceMethod(classToSwizzle, NSSelectorFromString(@"dealloc"), @selector(_RFKVOWrapperSwizzed_dealloc));
+        [RFSwizzledClasses() addObject:className];
+    };
+
+    // We swizzle the dealloc for both the object being observed and the observer of the observation. Because when either gets dealloc'd, we need to tear down the observation.
+    @synchronized (RFSwizzledClasses()) {
+        swizzle(self.class);
+        swizzle(observer.class);
+    }
+
+    return [[RFKVOTrampoline alloc] initWithTarget:self observer:observer keyPath:keyPath options:options queue:queue block:block];
+}
+
+- (BOOL)RFRemoveObserverWithIdentifier:(RFKVOTrampoline *)trampoline {
+    if (trampoline.target != self) return NO;
+    
+    [trampoline stopObserving];
+    return YES;
+}
+
+- (NSMutableSet *)RFKVOTrampolines {
+    return objc_getAssociatedObject(self, RFKVOTrampolinesKey);
+}
+
+- (void)setRFKVOTrampolines:(NSMutableSet *)set {
+    objc_setAssociatedObject(self, RFKVOTrampolinesKey, set, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end

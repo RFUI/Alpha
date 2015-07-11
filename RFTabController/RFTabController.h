23b92f3ea972ff3decfd5349b6c8b5f49417f6e3
BB9z <bb9z@me.com>
2015-07-11 11:42:54 +0800

    RFTabController, add methods to access view controller and index.

--- RFTabController/RFTabController.h
+++ RFTabController/RFTabController.h
@@ -16,10 +16,22 @@
 @interface RFTabController : UIViewController <
     RFInitializing
 >
+
+#pragma mark - Delegate & DataSource
 @property (weak, nonatomic) IBOutlet id<RFTabControllerDelegate> delegate;
 
+/**
+ RFTabController support set view controllers directly or provide view controllers through a data source. Ask the data source is preferred.
+ */
 @property (copy, nonatomic) NSArray *viewControllers;
 @property (weak, nonatomic) IBOutlet id<RFTabControllerDataSource> dataSource;
+
+#pragma mark - Index
+
+- (NSUInteger)indexForViewController:(UIViewController *)viewController;
+- (UIViewController *)viewControllerAtIndex:(NSUInteger)index;
+
+#pragma mark - Selection
 
 @property (weak, nonatomic) UIViewController *selectedViewController;
 @property (assign, nonatomic) NSUInteger selectedIndex;
@@ -27,8 +39,12 @@
 - (void)setSelectedIndex:(NSUInteger)index animated:(BOOL)animated completion:(void (^)(BOOL finished))completion;
 - (void)setSelectedViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(BOOL finished))completion;
 
-@property (weak, nonatomic) IBOutlet UIView *wrapperView;
+#pragma mark - View Config
 
+/**
+ The selected view controller’s view will be added into the wrapper view. If this property not set, the reciver will auto create a view fill it’s view’s bounds as the wrapper view.
+ */
+@property (weak, nonatomic) IBOutlet UIView *wrapperView;
 
 /**
  一般设置为 tab 按钮所在的容器，设置后会在切换 tab 时禁用 userInteractionEnabled，结束后启用

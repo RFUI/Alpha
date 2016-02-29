2dc7b69e9f6ea28c508d41d2f91e19ef1120ccb4
BB9z <bb9z@me.com>
2016-02-29 17:19:43 +0800

    Tidy RFGridView.

--- RFGridView/RFGridView.h
+++ RFGridView/RFGridView.h
@@ -2,57 +2,54 @@
     RFGridView
     RFUI
 
-    Copyright (c) 2012-2013 BB9z
+    Copyright (c) 2012-2013, 2016 BB9z
     https://github.com/RFUI/Alpha
 
     The MIT License (MIT)
     http://www.opensource.org/licenses/mit-license.php
-
-    BETA
-    ver 0.3.0
  */
 #import "RFUI.h"
 
 @class RFGridViewCellContainer;
 
-@interface RFGridView : UIScrollView
-- (id)initWithFrame:(CGRect)aRect;
-- (void)awakeFromNib;
+@interface RFGridView : UIScrollView <
+    RFInitializing
+>
 
-/// Called in initWithFrame: and awakeFromNib
-- (void)applyDefaultSettings;
+- (void)awakeFromNib;
 
 /// Views added to this view will layout as grid cell.
 /// It will be auto resize.
-@property (strong, nonatomic) IBOutlet RFGridViewCellContainer *container;
+@property (nonatomic, strong) IBOutlet RFGridViewCellContainer *container;
 
 /// ScrollView`s scroll direction
 @property (nonatomic) RFUIOrientation layoutOrientation;
-@property (nonatomic) BOOL layoutAnimated;
+@property (nonatomic) IBInspectable BOOL layoutAnimated;
 
-@property (nonatomic) CGSize cellSize;
-@property (nonatomic) RFMargin cellMargin;
-@property (nonatomic) RFPadding containerPadding;
+/// Default {20, 20}
+@property (nonatomic) IBInspectable CGSize cellSize;
+@property (nonatomic) RFEdge cellMargin;
+@property (nonatomic) RFEdge containerPadding;
 
-/** ScrollView padding
-    If you load RFGridView from a nib, it will set according to container`s frame when awakeFromNib called.
+/**
+ ScrollView padding
+
+ If you load RFGridView from a nib, it will set according to container`s frame when awakeFromNib called.
  */
-@property (nonatomic) RFPadding padding;
+@property (nonatomic) RFEdge padding;
 
 /// Leave for future use.
 /// Current is center.
-@property (nonatomic) RFAlignmentAnchor cellLayoutAlignment;
+@property (nonatomic) RFAlignmentAnchor cellLayoutAlignment __unavailable;
 
 /**
-    Call this after you modify cell's property.
-    You don`t need call this manully when grid view size changed.
+ Call this after you modify cell's property.
+ You don`t need call this manully when grid view size changed.
  */
 - (void)setNeedsLayout;
 
 @end
 
 @interface RFGridViewCellContainer : UIView
-@property (RF_WEAK, nonatomic) RFGridView *master;
+@property (nonatomic, weak) IBOutlet RFGridView *master;
 @end
-
-extern CGSize DEFAULT_RFGridViewCellSize;

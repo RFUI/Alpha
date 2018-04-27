215017f81ef4f98f188549e0a3b64b1780276425
BB9z <bb9z@me.com>
2018-04-27 18:31:26 +0800

    New RFCallbackControl.

---
+++ RFCallbackControl/RFCallbackControl.h
@@ -0,0 +1,64 @@
+/*!
+ RFCallbackControl
+ RFAlpha
+ 
+ Copyright © 2018 RFUI. All rights reserved.
+ https://github.com/RFUI/Alpha
+ 
+ Apache License, Version 2.0
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+#import <RFKit/RFRuntime.h>
+
+@class RFCallback;
+
+/**
+ 
+ */
+@interface RFCallbackControl<__covariant ObjectType>: NSObject
+
+@property (readonly) BOOL hasCallback;
+@property (nonnull) Class objectClass;
+
+- (nullable ObjectType)addCallbackWithTarget:(nonnull id)target selector:(nonnull SEL)selector refrenceObject:(nonnull id)object;
+
+- (nullable ObjectType)addCallback:(nonnull id)callback refrenceObject:(nonnull id)object;
+
+- (void)removeCallback:(nullable ObjectType)callbackRefrence;
+- (void)removeCallbackOfRefrenceObject:(nullable id)object;
+- (void)removeAllCallbacks;
+
+- (void)performWithSource:(nullable id)source filter:(NS_NOESCAPE BOOL (^__nullable)(ObjectType __nonnull obj, NSUInteger idx, BOOL *__nonnull stop))predicate;
+
+@end
+
+/**
+ 
+ */
+@interface RFCallback: NSObject
+
+/**
+ 一个弱引用的对象，当这个对象被释放后，callback 对象视为无效
+ */
+@property (nullable, weak) id refrenceObject;
+
+@property (nullable, weak) id target;
+
+/**
+ 非空时，向 target 发送 selector 消息，会带一个可为空的 source 参数
+ */
+@property (nullable) SEL selector;
+
+/**
+ 可变的 block 参数，当 target 或 selector 为空时调用
+ */
+@property (nullable) id block;
+
+/**
+ 子类重写
+ 
+ 因为回调 block 参数是可变的，如何传参只能看具体业务，默认无参数
+ */
+- (void)perfromBlock:(nonnull id)block source:(nullable id)source;
+
+@end
---
+++ RFCallbackControl/RFCallbackControl.m
@@ -0,0 +1,114 @@
+
+#import "RFCallbackControl.h"
+#import <RFKit/NSArray+RFKit.h>
+
+@interface RFCallback ()
+- (BOOL)_performFrom:(id)source;
+@end
+
+@interface RFCallbackControl ()
+@property (nonnull) NSMutableArray *_RFCallbackControl_callbacks;
+@end
+
+@implementation RFCallbackControl
+
+- (instancetype)init {
+    if (!(self = [super init])) return self;
+    _objectClass = _objectClass?: RFCallback.class;
+    __RFCallbackControl_callbacks = __RFCallbackControl_callbacks?: [NSMutableArray.alloc initWithCapacity:20];
+    return self;
+}
+
+- (BOOL)hasCallback {
+    return self._RFCallbackControl_callbacks.count > 0;
+}
+
+- (id)addCallbackWithTarget:(id)target selector:(SEL)selector refrenceObject:(id)object {
+    if (!object) return nil;
+    __kindof RFCallback *co = self.objectClass.new;
+    co.refrenceObject = object;
+    co.target = target;
+    co.selector = selector;
+    @synchronized(self) {
+        [self._RFCallbackControl_callbacks addObject:co];
+    }
+    return co;
+}
+
+- (id)addCallback:(id)callback refrenceObject:(id)object {
+    if (!object) return nil;
+    __kindof RFCallback *co = self.objectClass.new;
+    co.refrenceObject = object;
+    co.block = callback;
+    @synchronized(self) {
+        [self._RFCallbackControl_callbacks addObject:co];
+    }
+    return co;
+}
+
+- (void)removeCallback:(id)callbackRefrence {
+    if (!callbackRefrence) return;
+    @synchronized(self) {
+        [self._RFCallbackControl_callbacks removeObject:callbackRefrence];
+    }
+}
+
+- (void)removeCallbackOfRefrenceObject:(id)object {
+    if (!object) return;
+    @synchronized(self) {
+        [self._RFCallbackControl_callbacks removeObjectsPassingTest:^BOOL(RFCallback *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            return obj.refrenceObject == obj;
+        }];
+    }
+}
+
+- (void)removeAllCallbacks {
+    @synchronized(self) {
+        [self._RFCallbackControl_callbacks removeAllObjects];
+    }
+}
+
+- (void)performWithSource:(id)source filter:(BOOL (^)(id _Nonnull, NSUInteger, BOOL * _Nonnull))predicate {
+    NSArray *cbs = self._RFCallbackControl_callbacks;
+    @synchronized(self) {
+        if (predicate) {
+            NSIndexSet *is = [cbs indexesOfObjectsPassingTest:predicate];
+            cbs = [cbs objectsAtIndexes:is];
+        }
+        else {
+            cbs = cbs.copy;
+        }
+        
+        for (RFCallback *cb in cbs) {
+            if ([cb _performFrom:source]) continue;
+            [self._RFCallbackControl_callbacks removeObject:cb];
+        }
+    }
+}
+
+@end
+
+@implementation RFCallback
+
+- (BOOL)_performFrom:(id)source {
+    if (!self.refrenceObject) return NO;
+    id target = self.target;
+    SEL aSelector = self.selector;
+    if (target && aSelector) {
+        [UIApplication.sharedApplication sendAction:aSelector to:target from:source forEvent:nil];
+        return YES;
+    }
+    id cb = self.block;
+    if (cb) {
+        [self perfromBlock:cb source:source];
+        return YES;
+    }
+    return NO;
+}
+
+- (void)perfromBlock:(id)block source:(id)source {
+    dispatch_block_t cb = block;
+    cb();
+}
+
+@end

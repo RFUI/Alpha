7e7a24fb0ca36b47e415f176436cf1b4d7d538be
BB9z <bb9z@me.com>
2018-06-04 23:13:54 +0800

    RFFullSizeCollectionViewFlowLayout, simplify.

--- RFSliderView/RFFullSizeCollectionViewFlowLayout.m
+++ RFSliderView/RFFullSizeCollectionViewFlowLayout.m
@@ -6,17 +6,11 @@
 
 @implementation RFFullSizeCollectionViewFlowLayout
 
-- (instancetype)init {
-    if (!(self = [super init])) return nil;
-
+- (void)prepareLayout {
     self.sectionInset = UIEdgeInsetsZero;
     self.minimumInteritemSpacing = 0;
     self.minimumLineSpacing = 0;
     
-    return self;
-}
-
-- (void)prepareLayout {
     CGSize itemSize = self.collectionView.bounds.size;
     if (itemSize.width > 0 && itemSize.width < 10000 && itemSize.height > 0 && itemSize.height < 10000) {
         self.itemSize = itemSize;
@@ -28,9 +22,7 @@
 }
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
-    CGSize oldSize = self.collectionView.bounds.size;
-    if (!CGSizeEqualToSize(oldSize, newBounds.size)) {
-        self.itemSize = newBounds.size;
+    if (!CGSizeEqualToSize(self.itemSize, newBounds.size)) {
         return YES;
     }
     return NO;

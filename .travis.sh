dd0a36b1e52415d3c34c3bddf252000ac4877c61
BB9z <bb9z@me.com>
2020-01-02 16:10:28 +0800

    #ci config update.

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,23 @@
 #! /usr/bin/env sh
 
-set -eo pipefail
-# set -euxo pipefail
+set -euo pipefail
 
-echo $TRAVIS_COMMIT_MESSAGE
-echo "RFCI_TASK = $RFCI_TASK"
-readonly RFWorkspace="RFAlpha.xcworkspace"
+logInfo () {
+    echo "\033[32m$1\033[0m" >&2
+}
+
+logWarning () {
+    echo "\033[33m$1\033[0m" >&2
+}
+
+logError () {
+    echo "\033[31m$1\033[0m" >&2
+}
+
+logInfo "$TRAVIS_COMMIT_MESSAGE"
+logInfo "RFCI_TASK = $RFCI_TASK"
 readonly RFSTAGE="$1"
-echo "RFSTAGE = $RFSTAGE"
+logInfo "RFSTAGE = $RFSTAGE"
 
 # Run test
 # $1 scheme
@@ -26,26 +36,32 @@ XC_TestWatch() {
     xcodebuild build -workspace "$RFWorkspace" -scheme Target-watchOS ONLY_ACTIVE_ARCH=NO | xcpretty
 }
 
+STAGE_SETUP() {
+    gem install cocoapods --no-document
+}
+
 STAGE_MAIN() {
     if [ "$RFCI_TASK" = "POD_LINT" ]; then
         if [[ "$TRAVIS_COMMIT_MESSAGE" = *"[skip lint]"* ]]; then
-            echo "Skip pod lint"
+            logWarning "Skip pod lint"
         else
-            echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
-            gem install cocoapods --no-rdoc --no-ri --no-document --quiet
-            # Always allow warnings as alpha.
+            logInfo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+
+             # Always allow warnings as alpha.
             pod lib lint --allow-warnings
         fi
 
-    elif [ "$RFCI_TASK" = "Xcode9" ]; then
+    elif [ "$RFCI_TASK" = "Xcode10" ]; then
         pod install
+        XC_Test "Test-iOS" "platform=iOS Simulator,name=iPhone XS Max,OS=12.0"
+        XC_Test "Test-iOS" "platform=iOS Simulator,name=iPhone 5,OS=9.0"
     else
-        echo "Unexpected CI task: $RFCI_TASK"
+        logError "Unexpected CI task: $RFCI_TASK"
     fi
 }
 
 STAGE_SUCCESS() {
-    if [ "$RFCI_TASK" = "Xcode9" ]; then
+    if [ "${RFCI_COVERAGE-}" = "1" ]; then
         curl -s https://codecov.io/bash | bash -s
     fi
 }
